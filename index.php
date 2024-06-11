a58f74033708f6d6d105b26b6787574af9e1f322
AntonioDattolo <antonio19794.a@gmail.com>
2024-06-11 17:33:21 +0200

    BONUS 1

--- index.php
+++ index.php
@@ -1,50 +1,50 @@
-
 <?php
 
-    $hotels = [
+$hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
 
-    ];
+];
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -55,16 +55,29 @@
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <title>#####</title>
 </head>
+
 <body class="myBg">
-    
-<ul>
-    <?php
-    foreach($hotels as $hotel) {
-        echo "<li>" . $hotel["name"] . " Description: " . $hotel["description"] . " Parking: " . $hotel["parking"] .  " Vote: " . $hotel["vote"] . " Distance To Center: " . $hotel["distance_to_center"] . "</li>";
-    }
-    ?>
-</ul>
+
+    <section class="d-flex container justify-content-between flex-wrap">
+        <h1 class="col-12 text-center">Hotels</h1>
+        <div class="col-12">
+            <form action="ricerca.php" method="GET">
+
+                <select name="parking" >
+                    <option value="0">Assente</option>
+                    <option value="1">Presente</option>
+                    <option value="showAll">Mostra Tutti</option> 
+                </select>
+
+                <button type="submit">Invia</button>
+
+            </form>
+        </div>
+
      
-    
+    </section>
+
+
 </body>
+
 </html>
